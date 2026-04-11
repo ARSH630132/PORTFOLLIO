35463e60411455432df4568f3ea9434a2df99d97
ARSH SINGH <200009560+ARSH630132@users.noreply.github.com>
2026-04-11 18:07:08 +0530

    Add base path for portfolio in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PORTFOLLIO/', // YEH LINE ADD KAREIN. Isse path /PORTFOLLIO/src/main.tsx ban jayega.
+  base: '/PORTFOLLIO/', 
 })
